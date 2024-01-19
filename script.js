3b62b18b9bea6371f387c5f9b6a28159d9759145
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-19 15:21:16 +0530

    Add color text of msg

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
 
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissors"];
@@ -11,14 +15,20 @@ const genCompChoice = () => {
 
 const drawGame = () =>{
     console.log("Draw game");
+    msg.innerText = "Draw game";
 }
 
-const showWinner = (userWin) => {
+const showWinner = (userWin, userChoice, compChoice) => {
     if(userWin){
+        userScore++;
         console.log("You Win !!");
+        msg.innerText = `You Win !! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
     }
     else{
         console.log("You Lose !!");
+        msg.innerText = `You Lose !! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
     }
 }
 
@@ -42,7 +52,7 @@ const playGame = (userChoice) => {
             //rock, paper
             userWin = compChoice === "rock" ? false : true;
         }
-        showWinner(userWin);
+        showWinner(userWin , userChoice, compChoice);
     }
 
 };
@@ -51,7 +61,7 @@ choices.forEach((choice) =>{
     // console.log(choice);
     choice.addEventListener('click', () =>{
         const userChoice = choice.getAttribute("id");
-        console.log("Choice was clicked", userChoice);
+        // console.log("Choice was clicked", userChoice);
         playGame(userChoice);
     });
 });
